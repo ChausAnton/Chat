347ef04915057fecb23155fab4fd2b2b75e0876b
Anton Chaus <anchaus@c1r7p7.khpi.ucode-connect.study>
2021-01-15 14:32:31 +0200

    start of client and server

---
+++ Client/inc/Chat.h
@@ -0,0 +1,26 @@
+#pragma once 
+#ifndef chat
+#define chat
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ncurses.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+
+
+///server
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <time.h>
+
+
+////////gtk
+#include <gtk/gtk.h>
+
+#endif
