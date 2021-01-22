192098207d894c069312340ef7467fb39360bf9b
Anton Chaus <anchaus@c1r7p7.khpi.ucode-connect.study>
2021-01-22 16:48:48 +0200

    learning work with css

--- client_test_anch/src/main.c
+++ client_test_anch/src/main.c
@@ -5,9 +5,31 @@ void entry_text_change_event(GtkWidget *widget, GdkEventButton *event, gpointer 
     if (widget) {}
     if(event->type == GDK_BUTTON_PRESS && event->button == 1){
         char *name = (char*)gtk_entry_buffer_get_text(gtk_entry_get_buffer(GTK_ENTRY((GtkWidget*)search_field)));
-        printf("Added: %s\n", name);
+        printf("%s\n", name);
     }
 }
+
+void sing_in(GtkWidget *widget, GdkEventButton *event, gpointer *login[]) {
+    if (widget) {}
+    if(event->type == GDK_BUTTON_PRESS && event->button == 1){
+        char *name = (char*)gtk_entry_buffer_get_text(gtk_entry_get_buffer(GTK_ENTRY((GtkWidget*)login[0])));
+        printf("login: %s\n", name);
+        gtk_entry_set_text(GTK_ENTRY(login[0]), "");
+        char *passwrod = (char*)gtk_entry_buffer_get_text(gtk_entry_get_buffer(GTK_ENTRY((GtkWidget*)login[1])));
+        printf("passwrod: %s\n", passwrod);
+        gtk_entry_set_text(GTK_ENTRY(login[1]), "");
+    }
+}
+
+/*void sing_in_password(GtkWidget *widget, GdkEventButton *event, gpointer passwod) {
+    if (widget) {}
+    if(event->type == GDK_BUTTON_PRESS && event->button == 1){
+        char *name = (char*)gtk_entry_buffer_get_text(gtk_entry_get_buffer(GTK_ENTRY((GtkWidget*)passwod)));
+        printf("%s\n", name);
+        gtk_entry_set_text(GTK_ENTRY(passwod), "");
+    }
+}*/
+
 
 
 int main(int argc, char *argv[]) {
@@ -25,34 +47,46 @@ int main(int argc, char *argv[]) {
     gtk_css_provider_load_from_path(styles, "styles.css", NULL);
     gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(styles), GTK_STYLE_PROVIDER_PRIORITY_USER);
 
+    GtkWidget *activity = gtk_event_box_new();
+    gtk_widget_set_name(GTK_WIDGET(activity), "activity");                       // Имя
+    gtk_container_add(GTK_CONTAINER(window), activity);
+
     GtkWidget *activity_block = gtk_fixed_new();
-    gtk_widget_set_name(GTK_WIDGET(activity_block), "activity");                       // Имя
     gtk_widget_set_size_request(GTK_WIDGET(activity_block), 1400, 900);   // Размер
-    gtk_container_add(GTK_CONTAINER(window), activity_block);            // Позиция
+    gtk_container_add(GTK_CONTAINER(activity), activity_block);            // Позиция
+
+      
 
     GtkWidget *line = gtk_event_box_new();
     gtk_widget_set_name(GTK_WIDGET(line), "line");
     gtk_widget_set_size_request(GTK_WIDGET(line), 100, 900);
     gtk_fixed_put(GTK_FIXED(activity_block), line, 300, 0);
 
-    GtkWidget *button = gtk_event_box_new();
-    gtk_widget_set_name(GTK_WIDGET(button), "button");
-    gtk_widget_set_size_request(GTK_WIDGET(button), 280, 223);
-    gtk_fixed_put(GTK_FIXED(activity_block), button, 700, 450);
+    GtkWidget *enter_button = gtk_button_new_with_label ("Enter");
+    gtk_widget_set_name(GTK_WIDGET(enter_button), "enter_button");
+    gtk_button_set_relief(GTK_BUTTON(enter_button), GTK_RELIEF_NONE);
+    gtk_fixed_put(GTK_FIXED(activity_block), enter_button, 800, 750);
 
 //////////////
    /* GtkWidget *entry_block = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
     gtk_widget_set_name(GTK_WIDGET(entry_block), "entry_block");
     gtk_widget_set_size_request(GTK_WIDGET(entry_block), 100, 30);
     gtk_fixed_put(GTK_FIXED(activity_block),entry_block, 700, 450);*/
+    GtkWidget *sing_in_data[2];
+    sing_in_data[0] = gtk_entry_new();
+    gtk_widget_set_name(GTK_WIDGET(sing_in_data[0]), "login");
+    gtk_entry_set_placeholder_text(GTK_ENTRY(sing_in_data[0]), "Login:");
+    gtk_entry_set_max_length(GTK_ENTRY(sing_in_data[0]), 1000);
+    gtk_fixed_put(GTK_FIXED(activity_block), sing_in_data[0], 700, 450);
 
-    GtkWidget *entry_text = gtk_entry_new();
-    gtk_widget_set_name(GTK_WIDGET(entry_text), "entry_text");
-    gtk_entry_set_placeholder_text(GTK_ENTRY(entry_text), "Message ");
-    gtk_entry_set_max_length(GTK_ENTRY(entry_text), 1000);
-    gtk_fixed_put(GTK_FIXED(activity_block), entry_text, 700, 450);
+    sing_in_data[1] = gtk_entry_new();
+    gtk_widget_set_name(GTK_WIDGET(sing_in_data[1]), "Password");
+    gtk_entry_set_placeholder_text(GTK_ENTRY(sing_in_data[1]), "Password:");
+    gtk_entry_set_max_length(GTK_ENTRY(sing_in_data[1]), 1000);
+    gtk_fixed_put(GTK_FIXED(activity_block), sing_in_data[1], 700, 500);
 
-    g_signal_connect(G_OBJECT(button), "button_press_event", G_CALLBACK(entry_text_change_event), entry_text);
+    g_signal_connect(G_OBJECT(enter_button), "button_press_event", G_CALLBACK(sing_in), sing_in_data);
+    //g_signal_connect(G_OBJECT(button), "button_press_event", G_CALLBACK(sing_in_password), sing_in[1]);
 
 //////////////
 
