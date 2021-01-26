99bc6f4f79b5e481737b4e8fedcc0e9812175535
Alexander Lyannoy <alyannoy@c1r7p7.khpi.ucode-connect.study>
2021-01-26 17:15:19 +0200

    chat

---
+++ alex/src/main.c
@@ -0,0 +1,27 @@
+#include "Chat.h"
+
+
+int main(int argc, char *argv[]) {
+    GtkWidget *window;
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "NiChat");
+    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_SIZE_X, WINDOW_SIZE_Y);
+
+    GtkWidget *activity = gtk_event_box_new();
+    gtk_widget_set_name(GTK_WIDGET(activity), "activity");
+    gtk_container_add(GTK_CONTAINER(window), activity);
+
+    GtkWidget *activity_block = gtk_fixed_new();
+    gtk_widget_set_size_request(GTK_WIDGET(activity_block), WINDOW_SIZE_X, WINDOW_SIZE_Y);
+    gtk_container_add(GTK_CONTAINER(activity), activity_block);
+    
+    start_screen(&activity_block);
+    gtk_widget_show_all(window);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_main();
+    return 0;
+}
