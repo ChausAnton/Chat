efb36463080e9fb25597a8c628f7c1b1e03d9a03
Anton Chaus <anchaus@c1r7p7.khpi.ucode-connect.study>
2021-01-27 16:52:06 +0200

    add message and smile

--- client_test_anch/inc/Chat.h
+++ client_test_anch/inc/Chat.h
@@ -22,6 +22,7 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdbool.h>
+#include <math.h>
 
 ///server
 #include <sys/socket.h>
@@ -51,5 +52,8 @@ t_main_data main_data;
 void register_screen(GtkWidget *widget, GdkEventButton *event, gpointer **activity_bl);
 void start_screen(GtkWidget **activity_block);
 void main_screen(GtkWidget *widget, GdkEventButton *event, gpointer **activity_bl);
+
+gboolean draw_user_avatar(GtkWidget *widget, cairo_t *cr, char* path);
+gboolean draw_chat_avatar(GtkWidget *widget, cairo_t *cr, char* path);
 
 #endif
