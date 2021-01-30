963cb8dff58ba199ab02aa17a7c5513367d833de
Ilya Razumeyko <irazumeyko@c1r6p7.khpi.ucode-connect.study>
2021-01-30 20:41:09 +0200

    Added local gtk library and all necessaries for it. Changed Makefile. Changed headers for new libs

---
+++ client_test/resource/atk/.brew/atk.rb
@@ -0,0 +1,48 @@
+class Atk < Formula
+  desc "GNOME accessibility toolkit"
+  homepage "https://library.gnome.org/devel/atk/"
+  url "https://download.gnome.org/sources/atk/2.36/atk-2.36.0.tar.xz"
+  sha256 "fb76247e369402be23f1f5c65d38a9639c1164d934e40f6a9cf3c9e96b652788"
+
+  depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+
+  def install
+    mkdir "build" do
+      system "meson", "--prefix=#{prefix}", ".."
+      system "ninja"
+      system "ninja", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <atk/atk.h>
+
+      int main(int argc, char *argv[]) {
+        const gchar *version = atk_get_version();
+        return 0;
+      }
+    EOS
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    flags = %W[
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{include}/atk-1.0
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{lib}
+      -latk-1.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lintl
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
---
+++ client_test/resource/pango/include/pango-1.0/pango/pango.h
@@ -0,0 +1,50 @@
+/* Pango
+ * pango.h:
+ *
+ * Copyright (C) 1999 Red Hat Software
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __PANGO_H__
+#define __PANGO_H__
+
+#include "pango-attributes.h"
+#include "pango-bidi-type.h"
+#include "pango-break.h"
+#include "pango-context.h"
+#include "pango-coverage.h"
+#include "pango-direction.h"
+#include "pango-engine.h"
+#include "pango-enum-types.h"
+#include "pango-features.h"
+#include "pango-font.h"
+#include "pango-fontmap.h"
+#include "pango-fontset.h"
+#include "pango-glyph.h"
+#include "pango-glyph-item.h"
+#include "pango-gravity.h"
+#include "pango-item.h"
+#include "pango-layout.h"
+#include "pango-matrix.h"
+#include "pango-renderer.h"
+#include "pango-script.h"
+#include "pango-tabs.h"
+#include "pango-types.h"
+#include "pango-utils.h"
+#include "pango-version-macros.h"
+
+#endif /* __PANGO_H__ */
