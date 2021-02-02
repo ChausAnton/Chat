cf8a52986418922162d155991b867b6024ded0d8
Anton Chaus <anchaus@c1r7p7.khpi.ucode-connect.study>
2021-02-02 18:24:30 +0200

    add new type of image sending

--- anch_test/serv_test.c
+++ anch_test/serv_test.c
@@ -21,6 +21,128 @@
 
 #define SERVERPORT 8444
 
+static const unsigned char base64_table[65] =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+
+unsigned char * base64_encode(const unsigned char *src, size_t len,
+			      size_t *out_len)
+{
+	unsigned char *out, *pos;
+	const unsigned char *end, *in;
+	size_t olen;
+	int line_len;
+
+	olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
+	olen += olen / 72; /* line feeds */
+	olen++; /* nul termination */
+	if (olen < len)
+		return NULL; /* integer overflow */
+	out = malloc(olen);
+	if (out == NULL)
+		return NULL;
+
+	end = src + len;
+	in = src;
+	pos = out;
+	line_len = 0;
+	while (end - in >= 3) {
+		*pos++ = base64_table[in[0] >> 2];
+		*pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+		*pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+		*pos++ = base64_table[in[2] & 0x3f];
+		in += 3;
+		line_len += 4;
+		if (line_len >= 72) {
+			*pos++ = '\n';
+			line_len = 0;
+		}
+	}
+
+	if (end - in) {
+		*pos++ = base64_table[in[0] >> 2];
+		if (end - in == 1) {
+			*pos++ = base64_table[(in[0] & 0x03) << 4];
+			*pos++ = '=';
+		} else {
+			*pos++ = base64_table[((in[0] & 0x03) << 4) |
+					      (in[1] >> 4)];
+			*pos++ = base64_table[(in[1] & 0x0f) << 2];
+		}
+		*pos++ = '=';
+		line_len += 4;
+	}
+
+	if (line_len)
+		*pos++ = '\n';
+
+	*pos = '\0';
+	if (out_len)
+		*out_len = pos - out;
+	return out;
+}
+
+
+unsigned char * base64_decode(const unsigned char *src, size_t len,
+			      size_t *out_len)
+{
+	unsigned char dtable[256], *out, *pos, block[4], tmp;
+	size_t i, count, olen;
+	int pad = 0;
+
+	memset(dtable, 0x80, 256);
+	for (i = 0; i < sizeof(base64_table) - 1; i++)
+		dtable[base64_table[i]] = (unsigned char) i;
+	dtable['='] = 0;
+
+	count = 0;
+	for (i = 0; i < len; i++) {
+		if (dtable[src[i]] != 0x80)
+			count++;
+	}
+
+	if (count == 0 || count % 4)
+		return NULL;
+
+	olen = count / 4 * 3;
+	pos = out = malloc(olen);
+	if (out == NULL)
+		return NULL;
+
+	count = 0;
+	for (i = 0; i < len; i++) {
+		tmp = dtable[src[i]];
+		if (tmp == 0x80)
+			continue;
+
+		if (src[i] == '=')
+			pad++;
+		block[count] = tmp;
+		count++;
+		if (count == 4) {
+			*pos++ = (block[0] << 2) | (block[1] >> 4);
+			*pos++ = (block[1] << 4) | (block[2] >> 2);
+			*pos++ = (block[2] << 6) | block[3];
+			count = 0;
+			if (pad) {
+				if (pad == 1)
+					pos--;
+				else if (pad == 2)
+					pos -= 2;
+				else {
+					/* Invalid padding */
+					free(out);
+					return NULL;
+				}
+				break;
+			}
+		}
+	}
+
+	*out_len = pos - out;
+	return out;
+}
+
 int main(int argc, char *argv[]) {
     int socket_desc , client_sock , c , *new_sock;
 	struct sockaddr_in server , client;
@@ -54,18 +176,19 @@ int main(int argc, char *argv[]) {
 
 	int i = 0;
 	while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) ) {
-        
+        char p_array[1000];
 		printf("Reading Picture Size\n");
-                char p_array[1];
-                FILE *image = fopen("c1.png", "w");
-                int nb = read(client_sock, p_array, 1);
-                while (nb > 0) {
-                    fwrite(p_array, 1, nb, image);
-                    nb = read(client_sock, p_array, 1);
-                    printf("%d\n", nb);
-                }
-                fclose(image);
-                return 0;
+        FILE *image = fopen("c1.jpg", "wb");
+		read(client_sock, p_array, 1000);
+		int b64_size = atoi(p_array);
+		unsigned char b64[b64_size];
+		read(client_sock, b64, b64_size);
+		size_t b64_dec_len = b64_size * 3 / 4;
+		unsigned char *b64_dec = base64_decode(b64, b64_size, &b64_dec_len);
+		fwrite(b64_dec, b64_dec_len, 1, image);
+
+        fclose(image);
+        return 0;
 	}
 
 	close(socket_desc);
--- anch_test/test.c
+++ anch_test/test.c
@@ -21,6 +21,127 @@
 #define SERVERPORT 8444
 #define SERVERADDR "localhost"
 
+static const unsigned char base64_table[65] =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+unsigned char * base64_encode(const unsigned char *src, size_t len,
+			      size_t *out_len)
+{
+	unsigned char *out, *pos;
+	const unsigned char *end, *in;
+	size_t olen;
+	int line_len;
+
+	olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
+	olen += olen / 72; /* line feeds */
+	olen++; /* nul termination */
+	if (olen < len)
+		return NULL; /* integer overflow */
+	out = malloc(olen);
+	if (out == NULL)
+		return NULL;
+
+	end = src + len;
+	in = src;
+	pos = out;
+	line_len = 0;
+	while (end - in >= 3) {
+		*pos++ = base64_table[in[0] >> 2];
+		*pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+		*pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+		*pos++ = base64_table[in[2] & 0x3f];
+		in += 3;
+		line_len += 4;
+		if (line_len >= 72) {
+			*pos++ = '\n';
+			line_len = 0;
+		}
+	}
+
+	if (end - in) {
+		*pos++ = base64_table[in[0] >> 2];
+		if (end - in == 1) {
+			*pos++ = base64_table[(in[0] & 0x03) << 4];
+			*pos++ = '=';
+		} else {
+			*pos++ = base64_table[((in[0] & 0x03) << 4) |
+					      (in[1] >> 4)];
+			*pos++ = base64_table[(in[1] & 0x0f) << 2];
+		}
+		*pos++ = '=';
+		line_len += 4;
+	}
+
+	if (line_len)
+		*pos++ = '\n';
+
+	*pos = '\0';
+	if (out_len)
+		*out_len = pos - out;
+	return out;
+}
+
+
+unsigned char * base64_decode(const unsigned char *src, size_t len,
+			      size_t *out_len)
+{
+	unsigned char dtable[256], *out, *pos, block[4], tmp;
+	size_t i, count, olen;
+	int pad = 0;
+
+	memset(dtable, 0x80, 256);
+	for (i = 0; i < sizeof(base64_table) - 1; i++)
+		dtable[base64_table[i]] = (unsigned char) i;
+	dtable['='] = 0;
+
+	count = 0;
+	for (i = 0; i < len; i++) {
+		if (dtable[src[i]] != 0x80)
+			count++;
+	}
+
+	if (count == 0 || count % 4)
+		return NULL;
+
+	olen = count / 4 * 3;
+	pos = out = malloc(olen);
+	if (out == NULL)
+		return NULL;
+
+	count = 0;
+	for (i = 0; i < len; i++) {
+		tmp = dtable[src[i]];
+		if (tmp == 0x80)
+			continue;
+
+		if (src[i] == '=')
+			pad++;
+		block[count] = tmp;
+		count++;
+		if (count == 4) {
+			*pos++ = (block[0] << 2) | (block[1] >> 4);
+			*pos++ = (block[1] << 4) | (block[2] >> 2);
+			*pos++ = (block[2] << 6) | block[3];
+			count = 0;
+			if (pad) {
+				if (pad == 1)
+					pos--;
+				else if (pad == 2)
+					pos -= 2;
+				else {
+					/* Invalid padding */
+					free(out);
+					return NULL;
+				}
+				break;
+			}
+		}
+	}
+
+	*out_len = pos - out;
+	return out;
+}
+
 void mx_swap_char(char *s1, char *s2) {
     char temp = *s1;
     char temp2 = *s2;
@@ -73,7 +194,7 @@ char *mx_itoa(int number) {
     }
     
     char *arr = NULL;
-    arr = strnew(lenght);
+    arr = mx_strnew(lenght);
     bool min = false;
     num = number;
     int i = 0;
@@ -127,109 +248,24 @@ int main(int argc , char *argv[])
 	pthread_t sniffer_thread;
 	int *new_sock = malloc(1);
 	*new_sock = sock;
-	if( pthread_create( &sniffer_thread , NULL ,  read , (void*) new_sock) < 0) {
-		perror("could not create thread");
-		return 1;
-	}
 
-		//Get Picture Size
-                printf("Getting Picture Size\n");
-                FILE *picture;
-                picture = fopen(argv[1], "r");
-                int size;
+		FILE *picture;
+			picture = fopen(argv[1], "rb");
+			fseek(picture, 0, SEEK_END);
+			int size = ftell(picture);
+			fseek(picture, 0, SEEK_SET);
+            unsigned char buffer[size]; // no link between BUFSIZE and the file size
+            fread(buffer, size, 1, picture);
 
-                char send_buffer[1]; // no link between BUFSIZE and the file size
-                int nb = fread(send_buffer, 1, sizeof(send_buffer), picture);
-                while(!feof(picture)) {
-                        write(sock, send_buffer, nb);
-                        nb = fread(send_buffer, 1, sizeof(send_buffer), picture);
-                        // no need to bzero
-                }
-                printf("!!!!!!!!!!!!!!!!!!\n");
-                /*fseek(picture, 0, SEEK_END);
-                size=ftell(picture);
-                fseek(picture, 0, SEEK_SET);
+            size_t b64_len = 0;
+            unsigned char  *send_buffer = base64_encode(buffer, size, &b64_len);
 
-                printf("%d\n", size);
+            write(sock, mx_itoa(b64_len), strlen(mx_itoa(b64_len)));
+            write(sock, send_buffer, b64_len);
+			fclose(picture);
 
-                //Send Picture Size
-                printf("Sending Picture Size\n");
-                write(sock, mx_itoa(size) , strlen(mx_itoa(size)));
-
-                //Send Picture as Byte Array
-                printf("Sending Picture as Byte Array\n");
-                char send_buffer[size];
-                while(!feof(picture)) {
-                fread(send_buffer, sizeof(send_buffer), 1, picture);
-                write(sock, send_buffer, sizeof(send_buffer));
-                bzero(send_buffer, sizeof(send_buffer));
-                }*/
-                
-
-
-	pthread_join(sniffer_thread , NULL);
 	close(sock);
 	return 0;
 }
 
-//int main (int argc , char * argv [] ){
 
-
-
-
-
-        // argv[1] = 1.jpg
-
-       /* FILE *file;
-        char *buffer;
-        unsigned long fileLen;
-
-        //Open file
-        file = fopen(argv[1], "rb");
-        if (!file)
-        {
-                fprintf(stderr, "Unable to open file %s", argv[1]);
-                return 0;
-        }
-
-        //Get file length
-        fseek(file, 0, SEEK_END);
-        fileLen=ftell(file);
-        fseek(file, 0, SEEK_SET);
-
-        //Allocate memory
-        buffer=(char *)malloc(fileLen);
-        if (!buffer)
-        {
-                fprintf(stderr, "Memory error!");
-                                fclose(file);
-                return 1;
-        }
-
-       fread(buffer,fileLen,sizeof(char),file);
-       fclose(file);
-
-       int i=0;
-
-        FILE *write_ptr;
-        write_ptr = fopen("test.jpg","wb");  // w for write, b for binary
-        char temp[fileLen];
-       while (i < fileLen){
-           //fwrite(&buffer[i] ,sizeof(buffer[i]),1,write_ptr);
-           sprintf(&temp[i],"%02X",buffer[i]);
-           i++;
-           if( ! (i % 16) ) printf( "\n" );
-       }
-
-        while (i < fileLen){
-           fwrite(&temp[i] ,sizeof(temp[i]),1,write_ptr);
-           i++;
-           if( ! (i % 16) ) printf( "\n" );
-       }
-        
-    
-    fclose(write_ptr);
-
-        return 0;*/
-
-//}
